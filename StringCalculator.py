f8dbb4b364668b3493d52bf28d58b7e3eaa572dc
harshit <hrastogi43@gmail.com>
2022-05-19 02:37:26 +0530

    - negative sign test case added

--- StringCalculator.py
+++ StringCalculator.py
@@ -3,4 +3,33 @@ class StringCalculator():
     DELIMITER = ","
 
     def add(self, string):
-        return True
+        if string == "":
+            return 0
+        else:
+            return self.add_numbers(string)
+
+    def add_numbers(self, string):
+        return sum(self.extract_numbers_from(string))
+
+    def extract_numbers_from(self, string):
+        return [self.parse_number(number) for number in self.split_on_delimiters(string)]
+
+    def parse_number(self, number):
+        if "-" in number:
+            raise ValueError("Rejecting negative number")
+        return int(number)
+
+    def split_on_delimiters(self, string):
+        return self.normalize_delimiters(string).split(self.DELIMITER)
+
+    def normalize_delimiters(self, string):
+        normalized = string
+        normalized = self.handle_custom_delimiter(normalized)
+        normalized = normalized.replace("\n", self.DELIMITER)
+        return normalized
+
+    def handle_custom_delimiter(self, string):
+        if string.startswith("//"):
+            custom, rest = string.split("\n", 1)
+            return rest.replace(custom[2:], self.DELIMITER)
+        return string
--- calculatorTests.py
+++ calculatorTests.py
@@ -18,6 +18,14 @@ class TestStringCalculator(unittest.TestCase):
     def test_newlines_as_delimiter(self):
         self.assertEqual(calculator.add('2\n6'), 8)
 
+    def test_with_minus_signs_as_delimiter(self):  ## OK
+        self.assertEqual( calculator.add('//-\n1-2'), 3)
+
+    def test_rejects_negative_numbers(self):
+        self.assertRaises(ValueError, calculator.add, '-2')
+        self.assertRaises(ValueError, calculator.add, '-1,-2')
+
+
 
 
 if __name__ == '__main__':
