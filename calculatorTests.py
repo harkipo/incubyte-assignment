918a1caaaf18b9f118534bcd8aea41f949af7eab
harshit <hrastogi43@gmail.com>
2022-05-19 02:41:05 +0530

    calculator logic added and custom delimiter test cases added

--- calculatorTests.py
+++ calculatorTests.py
@@ -18,14 +18,20 @@ class TestStringCalculator(unittest.TestCase):
     def test_newlines_as_delimiter(self):
         self.assertEqual(calculator.add('2\n6'), 8)
 
-    def test_with_minus_signs_as_delimiter(self):  ## OK
+    def test_with_minus_signs_as_delimiter(self):
         self.assertEqual( calculator.add('//-\n1-2'), 3)
 
     def test_rejects_negative_numbers(self):
         self.assertRaises(ValueError, calculator.add, '-2')
         self.assertRaises(ValueError, calculator.add, '-1,-2')
 
+    def test_custom_delimiter(self):
+        self.assertEqual(calculator.add('//;\n5;2'), 7)
+        self.assertEqual(calculator.add('//+\n11+10'), 21)
+        self.assertEqual( calculator.add('//abc\n1abc2abc9'), 12)
 
+    def test_mixed_customDelimiter_and_defaultDelimiter(self):
+        self.assertEqual( calculator.add('//;\n1,2;3\n4'), 10)
 
 
 if __name__ == '__main__':
